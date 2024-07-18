394695632f50a29512d564fa7635657054c10af6
Carlos Núñez <c2vai2003@me.com>
2024-07-18 11:39:05 -0300

    Actualizacion S9

---
+++ Sesion 9/Clase/assets/javaScript/index.js
@@ -0,0 +1,14 @@
+const parrafo = document.getElementById("parrafo");
+/* 
+    Parrafo es la variable que guardara del documento (index.html) segun la id (getElementById) 
+    parrafo.
+
+    este valor es el elemento completo con id parrafo, este tiene atributos como por ejemplo el
+    contenido en texto y mostramos los atributos asi:
+*/
+console.log(parrafo.textContent);
+/*
+    con un punto y el atributo que quiera ver
+*/
+
+console.log(parrafo.innerHTML);
