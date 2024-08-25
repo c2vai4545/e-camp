0f27385e517b9ab9530c33bb9e10ab04c156f68d
Carlos Nuñez <carlosr.nunezt@gmail.com>
2024-08-25 18:26:35 -0300

    #comment Se corrige codigo y se agregan imagenes ya que la api no las proporciona.

--- Modulo 4/Prueba/assets/js/main.js
+++ Modulo 4/Prueba/assets/js/main.js
@@ -1,5 +1,9 @@
 import Categorias from './categorias.js';
+import Render from './render.js';
 
 const container = document.getElementById('heroes-container');
 const categorias = new Categorias(container);
+
 categorias.obtenerPersonajesYcategorias();
+const render = new Render(container);
+render.renderizarCategorias(categorias.obtenerCategorias());
